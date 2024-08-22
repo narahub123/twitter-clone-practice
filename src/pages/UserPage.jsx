793ede8b405a6d91f460a9b86fbaf67529f0f1e6
nara <parknara0213@gmail.com>
2024-08-22 15:28:13 +0900

    240822 유저 포스트 구현하기

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,33 @@
 import UserHeader from "../components/UserHeader";
+import UserPost from "./UserPost";
 
 const UserPage = () => {
   return (
     <>
       <UserHeader />
+      <UserPost
+        lilkes={1200}
+        replies={481}
+        postImage={"/post1.png"}
+        postTitle={"Let's talk about threads"}
+      />
+      <UserPost
+        lilkes={451}
+        replies={12}
+        postImage={"/post2.png"}
+        postTitle={"Nice tutorial"}
+      />
+      <UserPost
+        lilkes={321}
+        replies={989}
+        postImage={"/post3.png"}
+        postTitle={"I love this guy."}
+      />
+      <UserPost
+        lilkes={212}
+        replies={56}
+        postTitle={"This is my first thread."}
+      />
     </>
   );
 };
