f912c63c67897d0b69909d776a3c7abbf87839c9
nara <parknara0213@gmail.com>
2024-08-25 03:08:53 +0900

    240825 포스트 구현

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,54 @@
-import { Button, Flex } from "@chakra-ui/react";
-import React from "react";
-import { Link } from "react-router-dom";
+import { Flex, Spinner } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import Post from "./Post";
 
 const HomePage = () => {
+  const baseURL = import.meta.env.VITE_API_URL;
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(`${baseURL}/api/posts/feed`, {
+          credentials: "include",
+        });
+
+        const data = await res.json();
+
+        console.log(data);
+
+        if (data.error) {
+          return showToast("Error", data.error, "error");
+        }
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", error, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+    getFeedPosts();
+  }, [showToast]);
+
   return (
-    <Link to={`/markzucherberg`}>
-      <Flex w={"full"} justifyContent={"center"}>
-        <Button mx={"auto"}>visit Profile page</Button>
-      </Flex>
-    </Link>
+    <>
+      {loading && (
+        <Flex justifyContent={"center"}>
+          <Spinner size={"xl"} />
+        </Flex>
+      )}
+      {!loading && posts.length === 0 && (
+        <h1>Follow some users to see the feed</h1>
+      )}
+
+      {posts.map((post) => (
+        <Post key={post._id} post={post} postedBy={post.postedBy} />
+      ))}
+    </>
   );
 };
 
